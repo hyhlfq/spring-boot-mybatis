84f5559b8b07ca5a0af50dcdc4cfaeed9eb0f66e
黄怡皓 <howard.huang@clouclip.com>
2016-09-23 11:28:49 +0800

    添加了logback和相应的配置文件

--- src/main/java/cn/hyh/controller/v1/user/UserController.java
+++ src/main/java/cn/hyh/controller/v1/user/UserController.java
@@ -11,12 +11,24 @@ import java.util.ArrayList;
  * @Date : 2016/8/4 0004
  */
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 @RequestMapping(value = "/v1/users")
 public class UserController {
 
+    private static Logger log = LoggerFactory.getLogger(UserController.class);
+
     @RequestMapping(method = RequestMethod.GET)
     public ArrayList<User> getUser() {
+        log.trace("======trace");
+        log.debug("======debug");
+        log.info("======info");
+        log.warn("======warn");
+        log.error("======error");
+
         return new ArrayList<User>() {{
             add(new User(10000, "A", 21));
             add(new User(10001, "B", 22));
@@ -28,4 +40,3 @@ public class UserController {
         return new User(id, "HYH", 30);
     }
 }
-
